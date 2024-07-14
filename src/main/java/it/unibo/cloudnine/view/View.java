1d920acd6af91e8e6dfcb2e224f2c83fdf39f79e
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-07-14 17:34:02 +0200

    Added view

--- src/main/java/it/unibo/cloudnine/view/View.java
+++ src/main/java/it/unibo/cloudnine/view/View.java
@@ -1,5 +1,29 @@
 package it.unibo.cloudnine.view;
 
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+
+
 public class View {
-    
+
+    private final JFrame frame = new JFrame();
+    private final JTabbedPane pane = new JTabbedPane();
+
+    public View() {
+        frame.pack();
+        frame.setSize(new Dimension(600, 600));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        pane.addTab("Dio porco", new JPanel());
+        pane.addTab("Dio porco", new JPanel());
+        pane.addTab("Dio porco", new JPanel());
+
+        frame.add(pane);
+
+        frame.setVisible(true);
+    }
 }
