0b7a7aadd9606caa5a460a645e2f3a2a47dd3c48
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-07-14 18:21:43 +0200

    Added login tab

---
+++ src/main/java/it/unibo/cloudnine/view/LoginTab.java
@@ -0,0 +1,33 @@
+package it.unibo.cloudnine.view;
+
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextArea;
+
+import it.unibo.cloudnine.dao.UserManagementDAO;
+
+public final class LoginTab extends JPanel {
+
+    private final JButton loginButton = new JButton();
+    private final JTextArea usernameField = new JTextArea();
+    private final JPasswordField passwordField = new JPasswordField();
+    public LoginTab(final View view) {
+        this.setLayout(new GridLayout(4, 1));
+
+        this.add(new JLabel("Log in to the server!"));
+        this.add(usernameField);
+        this.add(passwordField);
+        this.add(loginButton);
+
+        loginButton.addActionListener(e -> {
+            if(UserManagementDAO.tryLogin(usernameField.getText(), String.valueOf(passwordField.getPassword()))) {
+                view.showUserTabs(usernameField.getText());
+            }
+        });
+
+    }
+}
