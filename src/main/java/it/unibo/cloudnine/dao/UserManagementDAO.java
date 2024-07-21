5f85c26771c0a1471ad6ab2b149302def4e3fa67
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-07-21 18:56:52 +0200

    Added query methods

--- src/main/java/it/unibo/cloudnine/dao/UserManagementDAO.java
+++ src/main/java/it/unibo/cloudnine/dao/UserManagementDAO.java
@@ -1,11 +1,20 @@
 package it.unibo.cloudnine.dao;
 
+import it.unibo.cloudnine.core.CloudnineManager;
+import it.unibo.cloudnine.core.DatabaseManager;
+
 public class UserManagementDAO {
+
+    private final DatabaseManager manager;
 
     public enum USER_TYPES {
         ADMIN,
         COOK,
         WAITER
+    }
+
+    public UserManagementDAO() {
+        manager = CloudnineManager.getDatabaseManager();
     }
     
     public static boolean tryLogin(final String user, final String password) {
@@ -14,5 +23,13 @@ public class UserManagementDAO {
 
     public static USER_TYPES getUserType(final String user) {
         return USER_TYPES.ADMIN;
+    }
+
+    public static void addAccount() {
+
+    }
+    
+    public static void removeAccount() {
+
     }
 }
