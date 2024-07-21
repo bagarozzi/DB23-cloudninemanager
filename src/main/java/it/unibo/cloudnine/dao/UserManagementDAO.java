a31bf2023a9759a645cac122c9779ffb8ac26cca
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-07-21 19:06:53 +0200

    Added log in query

--- src/main/java/it/unibo/cloudnine/dao/UserManagementDAO.java
+++ src/main/java/it/unibo/cloudnine/dao/UserManagementDAO.java
@@ -1,11 +1,19 @@
 package it.unibo.cloudnine.dao;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
 import it.unibo.cloudnine.core.CloudnineManager;
 import it.unibo.cloudnine.core.DatabaseManager;
 
 public class UserManagementDAO {
 
-    private final DatabaseManager manager;
+    private static final DatabaseManager manager;
+
+    static {
+        manager = CloudnineManager.getDatabaseManager();
+    }
 
     public enum USER_TYPES {
         ADMIN,
@@ -13,12 +21,21 @@ public class UserManagementDAO {
         WAITER
     }
 
-    public UserManagementDAO() {
-        manager = CloudnineManager.getDatabaseManager();
-    }
+    private static final String ACCOUNTS = "";
+    
+    private static final String LOGIN = "SELECT Password FROM Account WHERE username = ?";
     
     public static boolean tryLogin(final String user, final String password) {
-        return true;
+        try {
+            manager.openConnection();
+            List<Map<String, Object>> result = manager.getQuery(LOGIN, password);
+            if (result.size() == 1 && result.get(0).get("Password").equals(password)) {
+                return true;
+            }
+        } catch (SQLException e) {
+
+        }
+        return false;
     }
 
     public static USER_TYPES getUserType(final String user) {
