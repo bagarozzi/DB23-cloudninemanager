4da126bf18a0115b774b8c0f2b4d3fd35818f181
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-07-22 12:11:47 +0200

    Added bookings tab

---
+++ src/main/java/it/unibo/cloudnine/view/tabs/BookingsTab.java
@@ -0,0 +1,16 @@
+package it.unibo.cloudnine.view.tabs;
+
+import it.unibo.cloudnine.view.View;
+
+public class BookingsTab extends AbstractTab {
+    
+    public BookingsTab(final View view) {
+        super(view);
+    }
+
+    @Override
+    void refresh() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'refresh'");
+    }
+}
