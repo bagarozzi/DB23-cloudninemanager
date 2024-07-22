0b1b28785ef05b19e0482a25e0a923235552eb72
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-07-22 15:30:24 +0200

    Divide split and abstract

--- src/main/java/it/unibo/cloudnine/view/tabs/AbstractTab.java
+++ src/main/java/it/unibo/cloudnine/view/tabs/AbstractTab.java
@@ -1,14 +1,8 @@
 package it.unibo.cloudnine.view.tabs;
 
 import javax.swing.JButton;
-
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
-
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JSplitPane;
-import javax.swing.ScrollPaneConstants;
 
 import it.unibo.cloudnine.view.View;
 
@@ -17,22 +11,11 @@ public abstract class AbstractTab extends JPanel {
     public AbstractTab(final View view) {
         final JButton refreshButton = new JButton("Refresh");
         final JButton closeButton = new JButton("Close tab");
+        refreshButton.addActionListener(e -> refresh());
+        closeButton.addActionListener(e -> view.closeTab(this));
         this.setLayout(new BorderLayout());
         this.add(refreshButton, BorderLayout.NORTH);
         this.add(closeButton, BorderLayout.NORTH);
-
-        final JPanel itemsList = new JPanel(new GridLayout(0, 1));
-
-        final JScrollPane scrollpane = new JScrollPane(itemsList, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-        scrollpane.setWheelScrollingEnabled(true);
-
-        final JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scrollpane, new JPanel());
-        splitPane.setDividerLocation(0.5d);
-        splitPane.setResizeWeight(0.5d);
-        this.add(splitPane, BorderLayout.CENTER);
-
-        refreshButton.addActionListener(e -> refresh());
-        closeButton.addActionListener(e -> view.closeTab(this));
     }
 
     abstract void refresh();
---
+++ src/main/java/it/unibo/cloudnine/view/tabs/SplitViewTab.java
@@ -0,0 +1,27 @@
+package it.unibo.cloudnine.view.tabs;
+
+import it.unibo.cloudnine.view.View;
+
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.ScrollPaneConstants;
+import java.awt.GridLayout;
+
+public abstract class SplitViewTab extends AbstractTab {
+
+    public SplitViewTab(final View view) {
+        super(view);
+        final JPanel itemsList = new JPanel(new GridLayout(0, 1));
+
+        final JScrollPane scrollpane = new JScrollPane(itemsList, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollpane.setWheelScrollingEnabled(true);
+
+        final JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scrollpane, new JPanel());
+        splitPane.setDividerLocation(0.5d);
+        splitPane.setResizeWeight(0.5d);
+        this.add(splitPane, BorderLayout.CENTER);
+    }
+
+}
