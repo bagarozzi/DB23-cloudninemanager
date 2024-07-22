8e937c635f1a31da909626fe59ee2d698592821d
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-07-22 15:38:06 +0200

    Added setter methods

--- src/main/java/it/unibo/cloudnine/view/tabs/AbstractSplitViewTab.java
+++ src/main/java/it/unibo/cloudnine/view/tabs/AbstractSplitViewTab.java
@@ -7,21 +7,25 @@ import java.awt.BorderLayout;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.ScrollPaneConstants;
-import java.awt.GridLayout;
-
 public abstract class AbstractSplitViewTab extends AbstractTab {
+
+    private final JScrollPane scrollpane = new JScrollPane(new JPanel(), ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+    private final JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scrollpane, new JPanel());
 
     public AbstractSplitViewTab(final View view) {
         super(view);
-        final JPanel itemsList = new JPanel(new GridLayout(0, 1));
-
-        final JScrollPane scrollpane = new JScrollPane(itemsList, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         scrollpane.setWheelScrollingEnabled(true);
-
-        final JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scrollpane, new JPanel());
         splitPane.setDividerLocation(0.5d);
         splitPane.setResizeWeight(0.5d);
         this.add(splitPane, BorderLayout.CENTER);
+    }
+
+    public void setLeftPanel(final JPanel leftPanel) {
+        scrollpane.setViewportView(leftPanel);
+    }
+
+    public void setRightPanel(final JPanel rightPanel) {
+        splitPane.setRightComponent(rightPanel);
     }
 
 }
