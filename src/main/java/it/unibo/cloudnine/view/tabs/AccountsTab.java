111cae5b7b722fbba37c4eca658da3be42196bbf
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-07-25 13:27:02 +0200

    Added working account cancel

--- src/main/java/it/unibo/cloudnine/view/tabs/AccountsTab.java
+++ src/main/java/it/unibo/cloudnine/view/tabs/AccountsTab.java
@@ -2,6 +2,7 @@ package it.unibo.cloudnine.view.tabs;
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Color;
@@ -11,6 +12,9 @@ import javax.swing.border.LineBorder;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.JComboBox;
+
+import java.util.Vector; 
 
 import it.unibo.cloudnine.dao.UserManagementDAO;
 import it.unibo.cloudnine.data.Account;
@@ -24,6 +28,8 @@ public final class AccountsTab extends AbstractSplitViewTab {
     private final JTextField username = new JTextField();
     private final JTextField password = new JTextField();
     private final JTextField confirmPassword = new JTextField();
+    private final Vector<String> comboBoxVector = new Vector<>();
+    private final JComboBox<String> comboBox = new JComboBox<>(comboBoxVector);
 
     public AccountsTab(View view) {
         super(view);
@@ -35,19 +41,33 @@ public final class AccountsTab extends AbstractSplitViewTab {
 
     @Override
     void refresh() {
+        comboBoxVector.clear();
+        scrollingPane.removeAll();
         UserManagementDAO.getAllAccounts().forEach(account -> {
-            final JPanel element = new JPanel(new GridLayout(3, 2));
-            element.setMaximumSize(new Dimension(80, 2000));
-            element.add(new JLabel("username: " + account.username()));
-            element.add(getDeleteAccountButton(account));
+            final JPanel element = new JPanel(new GridBagLayout());
+            final GridBagConstraints c = new GridBagConstraints();
+            c.gridx = 0;
+            c.gridy = 0;
+            element.add(new JLabel("username: " + account.username()), c);
+            c.gridx = 1;
+            c.gridy = 0;
+            element.add(getDeleteAccountButton(account), c);
+            c.gridx = 0;
+            c.gridy = 1;
             element.add(new JLabel("utente: "
                 + account.name()
                 + " "
-                + account.surname()));
-            element.add(getChangePasswordButton(account));
-            element.add(new JLabel("Codice fiscale: " + account.codFisc()));
+                + account.surname()),
+            c);
+            c.gridx = 1;
+            c.gridy = 1;
+            element.add(getChangePasswordButton(account), c);
+            c.gridx = 0;
+            c.gridy = 2;
+            element.add(new JLabel("Codice fiscale: " + account.codFisc()), c);
             element.setBorder(new LineBorder(new Color(90, 93, 97), 3));
             scrollingPane.add(element);
+            comboBoxVector.add(account.codFisc());
         });
     }
 
@@ -56,27 +76,36 @@ public final class AccountsTab extends AbstractSplitViewTab {
         final GridBagConstraints c = new GridBagConstraints();
         c.gridx = 0;
         c.gridy = 0;
+        rightPane.add(new JLabel("Utente su cui fare la modifica: "), c);
+        c.gridx = 0;
+        c.gridy = 1;
         rightPane.add(new JLabel("Nome utente: "), c);
         c.gridx = 1;
-        c.gridy = 0;
-        rightPane.add(username, c);
-        c.gridx = 0;
-        c.gridy = 1;
-        rightPane.add(new JLabel("Password: "), c);
-        c.gridx = 1;
-        c.gridy = 1;
+        c.gridy = 2;
+        c.ipadx = 40;
         rightPane.add(password, c);
-        c.gridx = 0;
-        c.gridy = 2;
-        rightPane.add(new JLabel("Conferma password: "), c);
         c.gridx = 1;
-        c.gridy = 2;
+        c.gridy = 0;
+        rightPane.add(comboBox, c);
+        c.gridx = 1;
+        c.gridy = 1;
+        rightPane.add(username, c);
+        c.gridx = 1;
+        c.gridy = 3;
         rightPane.add(confirmPassword, c);
         c.gridx = 0;
+        c.gridy = 2;
+        c.ipadx = 0;
+        rightPane.add(new JLabel("Password: "), c);
+        c.gridx = 0;
         c.gridy = 3;
+        rightPane.add(new JLabel("Conferma password: "), c);
+        c.gridx = 0;
+        c.gridy = 4;
+        c.insets = new Insets(70, 20, 70, 0);
         rightPane.add(getClearFormButton(), c);
         c.gridx = 1;
-        c.gridy = 3 ;
+        c.gridy = 4;
         rightPane.add(getConfirmFormButton(), c);
     }
 
@@ -84,14 +113,16 @@ public final class AccountsTab extends AbstractSplitViewTab {
         final JButton button = new JButton("Cancella account");
         button.addActionListener(e -> {
             UserManagementDAO.removeAccount(account);
+            refresh();
         });
         return button;
     }
 
     private JButton getChangePasswordButton(final Account account) {
-        final JButton button = new JButton("Cancella account");
+        final JButton button = new JButton("Cambia password");
         button.addActionListener(e -> {
             UserManagementDAO.removeAccount(account);
+            refresh();
         });
         return button;
     }
