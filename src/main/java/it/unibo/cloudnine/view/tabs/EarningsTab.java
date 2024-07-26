44be7036bd656143e2b081e1bdf8f7b8e7934621
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-07-26 17:49:52 +0200

    Started earnings tab

--- src/main/java/it/unibo/cloudnine/view/tabs/EarningsTab.java
+++ src/main/java/it/unibo/cloudnine/view/tabs/EarningsTab.java
@@ -1,5 +1,15 @@
 package it.unibo.cloudnine.view.tabs;
 
-public class EarningsTab {
+import it.unibo.cloudnine.view.View;
+
+public class EarningsTab extends AbstractTab {
+
+    public EarningsTab(final View view) {
+        super(view);
+    }
+    @Override
+    void refresh() {
+
+    }
     
 }
