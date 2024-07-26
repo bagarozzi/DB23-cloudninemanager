ff83dc198fdb79d3336cb73be3d302175a734c8a
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-07-26 18:48:30 +0200

    corrected type

--- src/main/java/it/unibo/cloudnine/data/Menu.java
+++ src/main/java/it/unibo/cloudnine/data/Menu.java
@@ -4,6 +4,6 @@ package it.unibo.cloudnine.data;
  * A single instance of a menu with a name, a cost and a certain 
  * number of plates.
  */
-public record Menu(String nome, float costo, int numPiatti) {
+public record Menu(String nome, float costo, long numPiatti) {
 
 }
