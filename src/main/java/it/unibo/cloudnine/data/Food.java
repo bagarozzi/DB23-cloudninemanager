81517c7326151232f0faea52b31dc2d426318fae
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-08-08 15:32:02 +0200

    Overridden toString to represent graphically the object inside the ComboBoxes

--- src/main/java/it/unibo/cloudnine/data/Food.java
+++ src/main/java/it/unibo/cloudnine/data/Food.java
@@ -2,4 +2,8 @@ package it.unibo.cloudnine.data;
 
 public record Food(int codice, String name, String category, String type, float price) {
 
+    @Override
+    public final String toString() {
+        return codice + " - " + name;
+    }
 }
