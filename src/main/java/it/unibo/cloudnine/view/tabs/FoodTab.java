d65fa26068345875489fa516b41e4b60abf68dde
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-08-09 20:55:21 +0200

    Added working buttons

--- src/main/java/it/unibo/cloudnine/view/tabs/FoodTab.java
+++ src/main/java/it/unibo/cloudnine/view/tabs/FoodTab.java
@@ -13,11 +13,13 @@ import java.awt.Insets;
 import java.util.Vector;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
 import it.unibo.cloudnine.dao.FoodDAO;
 import it.unibo.cloudnine.dao.MenuDAO;
+import it.unibo.cloudnine.data.Food;
 import it.unibo.cloudnine.view.View;
 
 public class FoodTab extends AbstractSplitViewTab {
@@ -28,8 +30,20 @@ public class FoodTab extends AbstractSplitViewTab {
     
     private final JTextField foodName = new JTextField();
     private final JTextField foodCost = new JTextField();
-    private final Vector<String> comboBoxVector = new Vector<>();
-    private final JComboBox<String> comboBox = new JComboBox<>(comboBoxVector);
+
+    private final Vector<Food> comboBoxVector = new Vector<>();
+    private final JComboBox<Food> comboBox = new JComboBox<>(comboBoxVector);
+    
+    private final Vector<String> foodTypeVector = new Vector<>();
+    private final JComboBox<String> typeComboBox = new JComboBox<>(foodTypeVector);
+
+    private final Vector<String> categoryVector = new Vector<>();
+    private final JComboBox<String> categoryComboBox = new JComboBox<>(categoryVector);
+
+    private final JButton newFoodButton = new JButton("Nuovo piatto");
+    private final JButton confirmButton = new JButton("Conferma modifica");
+
+    private boolean isFormForNewFood = false;
 
     private final View view;
 
@@ -39,25 +53,68 @@ public class FoodTab extends AbstractSplitViewTab {
         initializeRightPanel();
         setLeftPanel(scrollingPane);
         setRightPanel(rightPane);
+
+        newFoodButton.addActionListener(e -> {
+            if(!isFormForNewFood) {
+                setFormAsNew();
+            }
+            else {
+                setFormAsExsisting();
+            }
+        });
+
+        confirmButton.addActionListener(e -> {
+            if(isFormForNewFood) {
+                FoodDAO.addFood(new Food(
+                    1,
+                    foodName.getText(),
+                    categoryVector.get(categoryComboBox.getSelectedIndex()),
+                    foodTypeVector.get(typeComboBox.getSelectedIndex()),
+                    Float.parseFloat(foodCost.getText())
+                ));
+            }
+            else {
+                FoodDAO.updateFood(new Food(
+                    comboBoxVector.get(comboBox.getSelectedIndex()).codice(),
+                    foodName.getText(),
+                    categoryVector.get(categoryComboBox.getSelectedIndex()),
+                    foodTypeVector.get(typeComboBox.getSelectedIndex()),
+                    Float.parseFloat(foodCost.getText())
+                ));
+                System.out.println(new Food(
+                    comboBoxVector.get(comboBox.getSelectedIndex()).codice(),
+                    foodName.getText(),
+                    categoryVector.get(categoryComboBox.getSelectedIndex()),
+                    foodTypeVector.get(typeComboBox.getSelectedIndex()),
+                    Float.parseFloat(foodCost.getText())
+                ));
+            }
+            refresh();
+        });
+
+        foodTypeVector.add("Piatto");
+        foodTypeVector.add("Bevanda");
+
         refresh();
     }
 
     @Override
     void refresh() {
         comboBoxVector.clear();
+        categoryVector.clear();
         scrollingPane.removeAll();
         FoodDAO.getAllFoods().forEach(food -> {
             final JPanel element = new JPanel(new GridBagLayout());
             final GridBagConstraints c = new GridBagConstraints();
             c.gridx = 1;
             c.gridy = 0; 
-            element.add(new JButton("dwesfgs"), c);
+            element.add(getModifyButton(food), c);
             c.gridx = 1;
             c.gridy = 1;
-            element.add(new JButton("dwesfgs"), c);
+            element.add(new JButton(""), c);
             c.gridx = 1;
             c.gridy = 2;
-            element.add(new JButton("dwesfgs"), c);
+            element.add(getRemoveFoodButton(food), c);
             c.gridx = 0;
             c.gridy = 0;
             c.insets = new Insets(0, 0, 0, 20);
@@ -70,7 +127,9 @@ public class FoodTab extends AbstractSplitViewTab {
             element.add(new JLabel("Prezzo: " + food.price() + " euro"), c);
             element.setBorder(new LineBorder(new Color(90, 93, 97), 3));
             scrollingPane.add(element);
+            comboBoxVector.add(food);
         });
+        categoryVector.addAll(FoodDAO.getAllCategories());
     }
 
     private void initializeRightPanel() {
@@ -78,12 +137,12 @@ public class FoodTab extends AbstractSplitViewTab {
         final GridBagConstraints c = new GridBagConstraints();
         c.gridx = 0;
         c.gridy = 0;
-        rightPane.add(new JLabel("Piatto: "), c);
+        rightPane.add(new JLabel("Vivanda: "), c);
         c.gridx = 0;
         c.gridy = 1;
-        rightPane.add(new JLabel("Nome del piatto: "), c);
+        rightPane.add(new JLabel("Nome della vivanda: "), c);
         c.gridx = 1;
-        c.gridy = 2;
+        c.gridy = 3;
         c.ipadx = 40;
         rightPane.add(foodCost, c);
         c.gridx = 1;
@@ -93,12 +152,67 @@ public class FoodTab extends AbstractSplitViewTab {
         c.gridy = 1;
         rightPane.add(foodName, c);
         c.gridx = 0;
-        c.gridy = 2;
+        c.gridy = 3;
         c.ipadx = 0;
-        rightPane.add(new JLabel("Costo del piatto: "), c);
+        rightPane.add(new JLabel("Costo della vivanda: "), c);
         c.gridx = 2;
         c.gridy = 0;
-        rightPane.add(new JButton("Nuovo piatto"), c);
+        rightPane.add(newFoodButton, c);
+        c.gridx = 0;
+        c.gridy = 2;
+        rightPane.add(new JLabel("Tipo di vivanda: "), c);
+        c.gridx = 1;
+        c.gridy = 2;
+        rightPane.add(typeComboBox, c);
+        c.gridx = 0;
+        c.gridy = 4;
+        rightPane.add(new JLabel("Categoria della vivanda"), c);
+        c.gridx = 1;
+        c.gridy = 4; 
+        rightPane.add(categoryComboBox, c);
+        c.gridx = 0;
+        c.gridy = 5;
+        c.gridwidth = 2;
+        rightPane.add(confirmButton, c);
+    }
 
+    private JButton getModifyButton(final Food food) {
+        final JButton button = new JButton("Modifica");
+        button.addActionListener(e -> {
+            setFormAsExsisting();
+            typeComboBox.setSelectedItem(food.type());
+            foodCost.setText(Float.toString(food.price()));
+            foodName.setText(food.name());
+            comboBox.setSelectedItem(food);
+            categoryComboBox.setSelectedItem(food.category());
+        });
+        return button;
+    }
+
+    private JButton getRemoveFoodButton(Food food) {
+        final JButton button = new JButton("Rimuovi");
+        button.addActionListener(e -> {
+            FoodDAO.removeFood(food);
+            refresh();
+        });
+        return button;
+    }
+
+    private void setFormAsExsisting() {
+        isFormForNewFood = false;
+        final GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 1;
+        c.gridy = 0;
+        rightPane.add(comboBox, c);
+        newFoodButton.setText("Nuovo piatto");
+        confirmButton.setText("Conferma modifica");
+    }
+
+    private void setFormAsNew() {
+        isFormForNewFood = true;
+        rightPane.remove(comboBox);
+        newFoodButton.setText("Piatto esistente");
+        confirmButton.setText("Aggiungi nuovo piatto");
+        this.revalidate();
     }
 }
