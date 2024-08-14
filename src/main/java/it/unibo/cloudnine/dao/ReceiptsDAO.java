06d4fdd20e985a3be154221edc38ee195f0a13d0
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-08-14 13:20:41 +0200

    Added total money query, not working tho

--- src/main/java/it/unibo/cloudnine/dao/ReceiptsDAO.java
+++ src/main/java/it/unibo/cloudnine/dao/ReceiptsDAO.java
@@ -1,7 +1,16 @@
 package it.unibo.cloudnine.dao;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+
+import com.google.common.base.Objects;
+import com.google.common.base.Optional;
+
 import it.unibo.cloudnine.core.CloudnineManager;
 import it.unibo.cloudnine.core.DatabaseManager;
+import it.unibo.cloudnine.data.StaffMember;
 
 public class ReceiptsDAO {
 
@@ -11,8 +20,18 @@ public class ReceiptsDAO {
         manager = CloudnineManager.getDatabaseManager();
     }
 
+    private static final String GET_EARNINGS = "SELECT SUM(comanda.Conto_finale) FROM comanda WHERE comanda.Data = CURRENT_DATE";
+
     public static float getTodaysEarnings() {
-        return 100.0f;
+        final List<Map<String, Object>> result = new ArrayList<>();
+        try {
+            manager.openConnection();
+            result.addAll(manager.getQuery(GET_EARNINGS));
+            System.out.println("diomerda" + result);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return (float)result.get(0).get("SUM(comanda.Conto_finale)");
     }
     
 }
